bd88b38c6480a1cfbbd1e6770bb1263353b58333
Donald Parish <donald.parish@gmail.com>
2012-02-19 15:36:21 -0600

    Refactor Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  match '/help',    to:  'static_pages#help'
+  match '/about',   to:  'static_pages#about'
+  match '/contact', to:  'static_pages#contact'
+
+  root to: 'static_pages#home'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -24,7 +27,7 @@ SampleApp::Application.routes.draw do
   #       post 'toggle'
   #     end
   #
-  #     collection do
+  #     collection do 
   #       get 'sold'
   #     end
   #   end
@@ -49,10 +52,6 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
